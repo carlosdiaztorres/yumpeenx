6735a6de82b9c20890fba6291ccb641b05ce7513
Carlos Diaz <carlosdiaz@CarlosDiaz.local>
2026-01-11 13:23:03 +0100

    Implement About Us page with portrait and synchronized navigation

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         portfolio: resolve(__dirname, 'portfolio.html'),
         services: resolve(__dirname, 'services.html'),
         contact: resolve(__dirname, 'contact.html'),
+        about: resolve(__dirname, 'about.html'),
       },
     },
   },
